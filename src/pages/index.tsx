124273db88568fb767c0d1c866a436806c714a8b
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-01-15 21:42:40 -0400

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import { Heading } from '@ignite-ui/react'
+import { makeHomePage } from '@/website/home/presentation/factories'
 
-export default function Home() {
-  return <Heading as="h1">Hello, brave new world!</Heading>
-}
+export default () => makeHomePage()
