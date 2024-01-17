fe4e15e91f995d37ac749ce92ec5cc73bfe6550a
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-01-17 00:22:59 -0400

    feat: add google permissions

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,34 @@
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+const SCOPES = [
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/calendar',
+]
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope: SCOPES.join(' '),
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ account }) {
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/connect-calendar?error=permissions'
+      }
+
+      return true
+    },
+  },
+}
+export default NextAuth(authOptions)
