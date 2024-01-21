84272c96d022e635c5d8e6664a55aab453fe8ced
gabrielauzier <gabrielvascoauzier@gmail.com>
2024-01-21 00:40:54 -0400

    fix: get week days params error

--- src/common/util/get-week-days.ts
+++ src/common/util/get-week-days.ts
@@ -2,7 +2,7 @@ interface GetWeekDaysParams {
   short?: boolean
 }
 
-export function getWeekDays({ short = false }: GetWeekDaysParams) {
+export function getWeekDays({ short }: GetWeekDaysParams = { short: false }) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from(Array(7).keys())
